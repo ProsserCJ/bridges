c72a4ffbfefb4ef96feb8b0eac4f1f79cf693d3f
barrymillham <j.mart.x93@gmail.com>
2014-04-27 17:37:51 -0400

    Manager List working

--- src/com/gccpod1/bentleybridges/DashboardActivity.java
+++ src/com/gccpod1/bentleybridges/DashboardActivity.java
@@ -1,38 +1,55 @@
 package com.gccpod1.bentleybridges;
 
+import java.util.ArrayList;
+
+
 import android.annotation.TargetApi;
 import android.app.ActionBar.LayoutParams;
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.GridView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashboardActivity extends Activity {
-		int numTestWidgets = 0;
-
+	int numTestWidgets = 0;
+	CheckboxListAdapter dataAdapter = null;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);		
+		
 		// Show the Up button in the action bar.		
 		//setupActionBar();
 
 		
-		GridView gridView = (GridView) findViewById(R.id.gridview);
-		gridView.setAdapter(new GraphAdapter(this));
-		gridView.setOnItemClickListener(null);
-		gridView.setVerticalSpacing(25);	
+		setUpTabs();
+							
+		displayDashboardContent();	
 		
+		displayManagerContent();
+	    
+	}
+
+	private void setUpTabs() {
 		TabHost th = (TabHost) findViewById(R.id.tabhost);
 		th.setup();
 		TabSpec spec = th.newTabSpec("arbitraryTag1");
@@ -44,39 +61,106 @@ public class DashboardActivity extends Activity {
 		spec.setContent(R.id.Manager);
 		spec.setIndicator("Manager");
 		th.addTab(spec);
-							
-		
-	    //final ActionBar actionBar = getActionBar();
-	    
-	    // Specify that tabs should be displayed in the action bar.
-	   /* actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	}
 
-	    // Create a tab listener that is called when the user changes tabs.
-	    ActionBar.TabListener tabListener = new ActionBar.TabListener() {
-	        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-	            // show the given tab
-	        }
-
-	        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-	            // hide the given tab
-	        }
-
-	        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-	            // probably ignore this event
-	        }
-	    };
-	    
-	        actionBar.addTab(
-	                actionBar.newTab()
-	                        .setText(R.string.dashboardtab1)
-	                        .setTabListener(tabListener));
-	        actionBar.addTab(
-	                actionBar.newTab()
-	                        .setText(R.string.dashboardtab2)
-	                        .setTabListener(tabListener)); */
-	    
+	private void displayDashboardContent() {
+		GridView gridView = (GridView) findViewById(R.id.gridview);
+		gridView.setAdapter(new GraphAdapter(this));
+		gridView.setOnItemClickListener(null);
+		gridView.setVerticalSpacing(25);
 	}
 	
+	 private void displayManagerContent() {
+		 
+		  //Array list of countries
+		  ArrayList<WidgetItem> widgetItemList = new ArrayList<WidgetItem>();
+		  widgetItemList.add(new WidgetItem("1", "NBI 59 Superstructure Condition Ratings", false));
+		  widgetItemList.add(new WidgetItem("2", "NBI 60 Substructure Condition Ratings", false));
+		  widgetItemList.add(new WidgetItem("3", "NBI 62 Culvert Condition Ratings", false));
+		  widgetItemList.add(new WidgetItem("4", "NHS Bridge Condition", false));
+		  widgetItemList.add(new WidgetItem("5", "Deck Area Bridge Condition", false));
+		  widgetItemList.add(new WidgetItem("6", "Deck Area NHS Bridge Condition", false));
+		  widgetItemList.add(new WidgetItem("7", "Structurally Deficient Deck Area", false));
+		  widgetItemList.add(new WidgetItem("8", "Structurally Deficient NHS Deck Area", false));
+		  widgetItemList.add(new WidgetItem("9", "Bridge Sufficiency Rating Deck Area", false));
+		  widgetItemList.add(new WidgetItem("10","NBI 58 Deck Condition Ratings", false));
+		  widgetItemList.add(new WidgetItem("11","Posted Bridges", false));
+		  widgetItemList.add(new WidgetItem("12", "Bridge Status", false));
+		  widgetItemList.add(new WidgetItem("13", "Bridge Condition", false));
+		  
+		  ListView listView = (ListView) findViewById(R.id.listView1);
+		  dataAdapter = new CheckboxListAdapter(this, R.layout.widget_item_info, widgetItemList);
+		  listView.setAdapter(dataAdapter);
+		 
+		  listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			  public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				  // When clicked, show a toast with the TextView text
+				  WidgetItem item = (WidgetItem) parent.getItemAtPosition(position);
+				  Toast.makeText(getApplicationContext(), "Clicked on Row: " + item.getContent() + ", Code: " + item.getCode(), 
+						  Toast.LENGTH_LONG).show();}
+		  });
+		  
+		  listView.setVerticalScrollBarEnabled(true);
+	 }
+	
+	private class CheckboxListAdapter extends ArrayAdapter<WidgetItem> {
+
+		private ArrayList<WidgetItem> widgetItemList;
+
+		public CheckboxListAdapter(Context context, int textViewResourceId, ArrayList<WidgetItem> widgList) {
+			super(context, textViewResourceId, widgList);
+			this.widgetItemList = new ArrayList<WidgetItem>();
+			this.widgetItemList.addAll(widgList);
+		}
+
+		private class ViewHolder {
+			TextView code;
+			CheckBox name;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+
+			ViewHolder holder = null;
+			Log.v("ConvertView", String.valueOf(position));
+
+			if (convertView == null) {
+				LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				convertView = vi.inflate(R.layout.widget_item_info, null);
+
+				holder = new ViewHolder();
+				holder.code = (TextView) convertView.findViewById(R.id.code);
+				holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+				convertView.setTag(holder);
+
+				holder.name.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						CheckBox cb = (CheckBox) v;
+						WidgetItem item = (WidgetItem) cb.getTag();
+						Toast.makeText(getApplicationContext(),
+								"Clicked on Checkbox: " + cb.getText() + " is "
+										+ cb.isChecked(), Toast.LENGTH_LONG).show();
+						item.setSelected(cb.isChecked());
+					}
+				});
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+
+			WidgetItem widgetItem = widgetItemList.get(position);
+			//holder.code.setText(" (" + widgetItem.getCode() + ")"); //UNCOMMENT this to view the widget codes
+			holder.code.setText("");
+			holder.name.setText("    " + widgetItem.getContent());
+			holder.name.setChecked(widgetItem.isSelected());
+			holder.name.setTag(widgetItem);
+
+			return convertView;
+
+		}
+
+	}
+	 
+	 
 	protected void onResume()
 	{
 		super.onResume();	
